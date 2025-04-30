4b4779d9f3c175c9e6141f8bcd85c6d8e90d4ef8
Pramish K.C <pramishkc066@gmail.com>
2025-04-30 20:04:46 +0545

    accessing models

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,6 +10,15 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False, db_index=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("detail", args=[self.slug])
+    
 
     def __str__(self):
-        return f"{self.title} {self.rating} {self.author} {self.is_bestselling}"
+        return f"{self.title} {self.rating} {self.author} {self.is_bestselling} {self.slug}"
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import Book
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.db.models import Avg
 
 # Create your views here.
+def index(request):
+    books = Book.objects.all()
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))['rating__avg']
+    return render(request, "book_outlet/index.html", {'books': books,
+                                                      'total_number_of_books':num_books,
+                                                      'average_rating':int(avg_rating)})
+
+def book_detail(request, slug):
+    try:
+        book = get_object_or_404(Book, slug=slug)
+    except:
+        raise Http404()
+    
+    return render(request, "book_outlet/book_detail.html", {
+        'id': book.id,
+        'title': book.title,
+        'author': book.author,
+        'rating': book.rating,
+        'is_bestseller': book.is_bestselling,
+    })
+
