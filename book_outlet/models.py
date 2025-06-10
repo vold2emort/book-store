ec8c8a5847042be0acec5020a6b9fc7f3b362634
Pramish K.C <pramishkc066@gmail.com>
2025-06-10 21:47:40 +0545

    updated data models

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,11 +5,28 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}, {self.code}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 
 class Address(models.Model):
     street = models.CharField(max_length=80)
     postal_code = models.CharField(max_length=5)
     city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
+        db_table_comment = "Address of Authors"
     
 
 class Author(models.Model):
@@ -17,20 +34,17 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     addess = models.OneToOneField(Address, on_delete=models.CASCADE, null=True)
 
-
-    def full_name(self):
-        return f"{self.first_name} {self.last_name}"
-    
     def __str__(self):
-        return self.full_name
+        return f"{self.first_name}, {self.last_name}, {self.addess}"
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="",blank=True ,  null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     # def save(self, *args, **kwargs):
     #     self.slug = slugify(self.title)
